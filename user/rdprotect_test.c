1c1a038cc94681ae22b16c4908c491e90308cc21
basti <charley.napa69@gmail.com>
2025-11-30 23:05:48 -0300

    Agregar programa de prueba rdprotect_test.c

---
+++ user/rdprotect_test.c
@@ -0,0 +1,25 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int main() {
+    char *addr = sbrk(0);
+    sbrk(4096);
+    addr[0] = 'Z';
+    
+    if (mrdprotect(addr, 1) < 0) {
+        printf("mrdprotect falló\n");
+        exit(1);
+    }
+    
+    addr[0] = 'A';
+    char c = addr[0];
+    printf("Valor leido: %c\n", c);
+    
+    if (munrdprotect(addr, 1) < 0) {
+        printf("munrdprotect falló\n");
+        exit(1);
+    }
+    printf("Protección revertida\n");
+    exit(0);
+}
